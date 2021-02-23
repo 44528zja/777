d9193dfda6b60d734f07f2d9ce7f1d05cdbe0536
44528zja <79467027+44528zja@users.noreply.github.com>
2021-02-23 19:46:02 +0800

    Update js/index.js

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;
     var pwd=document.getElementById("pwd").value;
 		//修改密码请改此处
-    if(userName=="黄小华" &&  pwd=="0226"){
+    if(userName=="黄小华" &&  pwd=="0115"){
 			event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
